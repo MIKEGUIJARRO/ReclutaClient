48d44ff3c2c98901f443107c446220392ff0c1af
Mike Guijarro <a01612042@itesm.mx>
2023-01-01 22:29:11 -0600

    Add Positions component

--- src/pages/Home/Positions/Positions.tsx
+++ src/pages/Home/Positions/Positions.tsx
@@ -1,14 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { CardContainer } from '../../../components/pages/positions/CardContainer';
+import { ReclutaAPI } from '../../../services/reclutaAPI';
 
 export const Positions = () => {
+  const reclutaAPI = new ReclutaAPI();
+  const { data, isLoading, error } = useQuery(['positions'], async () => {
+    const response = await reclutaAPI.positions('findAll', {});
+    return response;
+  });
+
   return (
     <div className="">
       <h1 className="font-bold text-4xl">Posiciones</h1>
       <div className="divider"></div>
-      <div>
-        <CardContainer />
-      </div>
+      <div>{data?.success && <CardContainer data={data?.data} />}</div>
     </div>
   );
 };
