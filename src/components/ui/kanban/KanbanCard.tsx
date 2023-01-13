b68f8b2a0aa544c95e363a826b1ac017d6cb5c5b
Mike Guijarro <a01612042@itesm.mx>
2023-01-12 21:56:01 -0600

    Add KanbanCard

---
+++ src/components/ui/kanban/KanbanCard.tsx
@@ -0,0 +1,48 @@
+import React, { FC, ReactElement } from 'react';
+import { Item, KanbanCardContent } from './KanbanBoard';
+import { Draggable } from 'react-beautiful-dnd';
+
+interface KanbanCard {
+  item: Item<any>;
+  index: number;
+  kanbanCardContent: ReactElement<KanbanCardContent<any>>;
+}
+
+export const KanbanCard: FC<KanbanCard> = ({
+  item,
+  index,
+  kanbanCardContent,
+}) => {
+  const renderContent = () => {
+    if (React.isValidElement(kanbanCardContent)) {
+      return React.cloneElement(kanbanCardContent, {
+        id: item.id,
+        index: index,
+        content: item.content,
+      });
+    }
+  };
+
+  return (
+    <Draggable draggableId={item.id} index={index} isDragDisabled={false}>
+      {(provided, snapshot) => {
+        return (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            className={'mb-4'}
+          >
+            <div
+              className={`bg-white p-4 rounded-md transition-color duration-500 outline-dashed outline-transparent outline-2 ${
+                snapshot.isDragging ? 'outline-base-300 shadow-2xl' : ''
+              }`}
+            >
+              {renderContent()}
+            </div>
+          </div>
+        );
+      }}
+    </Draggable>
+  );
+};
