4323a9d4c644cd8d4491bc4395d8200baa947e06
Mike Guijarro <a01612042@itesm.mx>
2023-01-18 21:14:03 -0600

    Add tailwind config plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("@tailwindcss/line-clamp")],
+  plugins: [
+    require("daisyui"),
+    require("@tailwindcss/line-clamp"),
+    require('@tailwindcss/typography'),
+  ],
   daisyui: {
     themes: ["winter"],
     darkTheme: false,
