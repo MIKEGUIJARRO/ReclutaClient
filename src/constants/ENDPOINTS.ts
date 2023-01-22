0910fe1cfd264116feee383d6ef4da57f5563f7e
Mike Guijarro <a01612042@itesm.mx>
2023-01-22 15:29:16 -0600

    Refactor Endpoint production

--- src/constants/ENDPOINTS.ts
+++ src/constants/ENDPOINTS.ts
@@ -2,5 +2,5 @@ export const ENDPOINTS = {
   reclutaHostNameURL:
     import.meta.env.MODE === 'development'
       ? 'http://localhost:5000'
-      : 'http://54.166.97.9',
+      : 'http://www.recluta.io/',
 };
